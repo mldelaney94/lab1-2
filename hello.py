577ce30d1d560807ea19a441ea20b149514881cc
z3463382 <mldelaney94@gmail.com>
2017-08-04 10:56:17 +1000

    Added fizzbuzz function

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello World")
+n = int(input())
+def fizz_buzz (n):
+    for i in range (0, n+1): #displays up to number entered
+        msg = ''
+        if i % 3 == 0:
+            msg += "Fizz"
+        if i % 5 == 0:
+            msg += "Buzz"
+        print (msg or i)
