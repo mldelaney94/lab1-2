c93e80a43f0aa89c7f9f33316d2b16ab6bfd1cde
z3463382 <mldelaney94@gmail.com>
2017-08-04 10:58:21 +1000

    Changed fizzbuzz in subbranch

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 print("Hello World")
-n = int(input())
-def fizz_buzz (n):
-    for i in range (0, n+1): #displays up to number entered
+def fizz_buzz ():
+    for i in range (0, 100): #displays up to number entered
         msg = ''
         if i % 3 == 0:
             msg += "Fizz"
